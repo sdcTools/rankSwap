b60c3afbc5f1bd504e5138767a0741701df0b024
Johannes Gussenbauer - QM <gussenbauer@xlwt0006.statistik.local>
2018-05-23 09:45:51 +0200

    dummy code

---
+++ R/dummy_R_Code.R
@@ -0,0 +1,16 @@
+#######################################################
+# dummy R-Code which will be exported to C++
+#
+#
+
+#
+# lies frame ein (vorerst....)
+# bearbeite Frame auch mit simPop (-> still to do...)
+library(data.table)
+library(sampSTAT)
+
+dat.frame <- getFrame(ageMin=16,ageMax=60,sample.date="2017-12-01")
+
+
+
+
