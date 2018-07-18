73dced61323ec182a52c5a60ec288773584b6e9b
Johannes Gussenbauer - QM <gussenbauer@xlwt0006.statistik.local>
2018-07-18 09:00:24 +0200

    split up file for use in R and single cpp-header file without R-dependencies

--- R/test_recordSwap.R
+++ R/test_recordSwap.R
@@ -1,7 +1,157 @@
-#################################################
+##########################################################
+# Test-File for record swapping
+# tests for all functions in recordSwap.h
 #
-# test recordSwapping
+
+
+library(data.table)
+library(Rcpp)
+library(wrswoR)
+set.seed(1234)
+
+source("R/create_dat.R")
+sourceCpp("src/recordSwap.cpp")
+
+dat <- create.dat(100000)
+
+###############################
+# test set level function
+# test if outcome is same
+test.fun <- function(){
+  dat <- create.dat()
+  hierarchy <- c("nuts1","nuts2","nuts3","nuts4")
+  risk <- c("hsize","ageGroup","geschl","national")
+  th <- 3
+  dat[,level:=0]
+  for(i in 1:4){
+    dat[level==0,N:=.N,by=c(hierarchy[1:i],risk)]
+    dat[level==0,level:=as.numeric(i*(N<=th))]
+  }
+  dat[level==0,level:=5]
+  dat[,level:=min(level),by=hid]
+  
+  dat[,test:=setLevels(dat,0:3,5:8,4,3)+1]
+  
+  if(nrow(dat[test!=level])>0){
+    return(FALSE)
+  }else{
+    return(TRUE)
+  }
+}
+
+a <- replicate(1000,test.fun())
+table(a)
+
+###############################
+
+###############################
+# test ordering function
+test <- replicate(1000,{
+  dat <- create.dat()
+  dat_orig <- copy(dat)
+  dat <- dat[sample(1:.N)]
+  dat <- orderData(dat,4)
+  dat <- as.data.table(dat)
+  setnames(dat,colnames(dat),colnames(dat_orig))
+  all.equal(dat[,.(hid)],dat_orig[,.(hid)])
+})
+
+table(test)
+###############################
+
+###############################
+# test sampling function
+sourceCpp("src/recordSwap.cpp")
+n <- 100000
+N <- 1000
+b <- 1e4
+ID <- 1:n
+seed.base <- 1:1e6
+
+prob <- sample(c(rnorm(n/2,mean=10,sd=2),rnorm(n/2,mean=100,sd=20)))
+out_mat <- replicate(b,{
+  # cpp_samp <- randSample(1:n,N,prob,seed=sample(seed.base,1))
+  R_samp <- sample(1:n,N,prob=prob)
+  wrswoR_samp <- sample_int_crank(n,N,prob)
+  cbind(#cpp_samp,
+    R_samp,wrswoR_samp)
+})
+out_cpp <- test_randSample(b,1:n,N,prob,seed=1)
+
+out_data <- list()
+for(i in 1:b){
+  out_tmp <- c(out_mat[,1:2,i],out_cpp[[i]])
+  out_tmp <- data.table(value=out_tmp,run=i)
+  out_tmp[,method:=rep(c("R_samp","wrswoR_samp","cpp_samp"),each=N)]
+  out_data <- c(out_data,list(out_tmp))
+}
+
+out_data <- rbindlist(out_data)
+
+library(ggplot2)
+ggplot(out_data,aes(value))+
+  geom_density(aes(color=method))
+###############################
+
+###############################
+# test swapping outcome
 #
+sourceCpp("src/recordSwap.cpp")
+b <- 500
+all_hid <- rep(FALSE,b)
+all_swapped <- rep(FALSE,b)
+
+for(i in 1:b){
+  dat <- create.dat(50000)
+  dat_R <- copy(dat)
+  levels <- setLevels(dat,0:3,5:8,4,3)
+  prob <- setRisk(dat,0:3,5:8,4)
+  dat_R[,level:=levels]
+  dat_R[,risk:=prob[[1]]]
+  dat_R[,antirisk:=prob[[2]]]
+  dat_R <- unique(dat_R,by="hid")
+  hierarchy <- c("nuts1","nuts2","nuts3","nuts4")
+  swap <- .1
+  for(i in 1:length(hierarchy)){
+    if(i==1){
+      dat.draw <- dat_R[,.(drawN=randomRound(.N*swap)),by=c(hierarchy[1:i],"hsize")]
+    }else{
+      # distribute N
+      # take number of risky households into account??????!!!!!!!!
+      dat.draw.next <- dat_R[,.(N=as.numeric(.N)),by=c(hierarchy[1:i],"hsize")]
+      dat.draw.next[,N:=N/sum(N),by=c("hsize",hierarchy[1:(i-1)])]
+      dat.draw <- merge(dat.draw,dat.draw.next,by=c("hsize",hierarchy[1:(i-1)]),all.y=TRUE)
+      dat.draw[,drawN:=randomRound(N*drawN),by=c(hierarchy[1:i],"hsize")]
+      dat.draw[,N:=NULL]
+    }
+  }
+  dat_R <- merge(dat_R,dat.draw,by=c(hierarchy,"hsize"))
+  
+  swap_R <- recordSwapR(copy(dat_R),hierarchy)
+  swap_cpp <- as.data.table(recordSwap(dat,5,0:3,5:8,4,3,.1))
+  setnames(swap_cpp,colnames(swap_cpp),colnames(dat))
+  
+  swap_cpp[,comb_hier:=paste(nuts1,nuts2,nuts3,nuts4)]
+  if(nrow(swap_cpp[,uniqueN(comb_hier),by=hid][V1>1])>0){
+    all_hid[i] <- TRUE
+  }
+  
+  dat[,levels:=levels]
+  orig_id <- unique(dat[levels<length(hierarchy)-1,.(hid,orig=paste(nuts1,nuts2,nuts3,nuts4))])
+  swap_cpp[,levels:=levels]
+  changed_id <- unique(swap_cpp[levels<length(hierarchy)-1,.(hid,new=paste(nuts1,nuts2,nuts3,nuts4))])
+  id_was_swapped <- merge(orig_id,changed_id,by="hid")
+  if(nrow(id_was_swapped[orig==new])>0){
+    all_swapped[i] <- TRUE
+  }
+}
+table(all_swapped)
+table(all_hid)
+
+
+
+sourceCpp("src/recordSwap.cpp")
+swap_cpp <- recordSwap(dat,5,0:3,5:8,4,3,.1)
 
 library(data.table)
 library(Rcpp)
@@ -17,7 +167,6 @@ sourceCpp("diverses/recordSwap.cpp")
 # Test c++ functions
 setLevels(dat,0:2,3,0:2,1,3)
 
-
 hierarchy <- c("nuts1","nuts2","nuts3")
 th <- 3
 dat[,level:=0]
@@ -31,37 +180,10 @@ dat[,test:=setLevels(dat,0:2,3,0:2,1,3)+1]
 
 dat[1:100,.N,by=list(nuts1,nuts2,nuts3,hsize)]
 
-# test if outcome is same
-test.fun <- function(){
-  dat <- create.dat()
-  hierarchy <- c("nuts1","nuts2","nuts3")
-  th <- 3
-  dat[,level:=0]
-  for(i in 1:3){
-    dat[level==0,N:=.N,by=c(hierarchy[1:i],"hsize")]
-    dat[level==0,level:=as.numeric(i*(N<=th))]
-  }
-  dat[level==0,level:=4]
-  
-  dat[,test:=setLevels(dat,0:2,3,0:2,1,3)+1]
-  
-  if(nrow(dat[test!=level])>0){
-    return(FALSE)
-  }else{
-    return(TRUE)
-  }
-}
 
-a <- replicate(1000,test.fun())
 
 dat <- create.dat(1000000)
-library(microbenchmark)
-microbenchmark(cpp=setLevels(dat,0:2,3,0:2,1,3),
-               R={dat[,level:=0];
-                 for(i in 1:3){
-                   dat[level==0,N:=.N,by=c(hierarchy[1:i],"hsize")]
-                   dat[level==0,level:=as.numeric(i*(N<=th))]
-                 }})
+
 
 
 #
@@ -69,17 +191,7 @@ microbenchmark(cpp=setLevels(dat,0:2,3,0:2,1,3),
 #
 sourceCpp("diverses/recordSwap.cpp")
 
-test <- replicate(1000,{
-  dat <- create.dat()
-  dat_orig <- copy(dat)
-  dat <- dat[sample(1:.N)]
-  dat <- orderData(dat,4)
-  dat <- as.data.table(dat)
-  setnames(dat,colnames(dat),colnames(dat_orig))
-  all.equal(dat,dat_orig)
-})
 
-table(test)
 
 
 dat <- create.dat()
@@ -427,6 +539,30 @@ vec1 <- 1:1000000
 
 test_stuff(vec1)
 microbenchmark(vec1)
+
+
+###################################################
+# create test battery for comparison with R-Code
+#
+#
+# test ordering
+#
+sourceCpp("diverses/recordSwap.cpp")
+
+test <- replicate(1000,{
+  dat <- create.dat()
+  dat_orig <- copy(dat)
+  dat <- dat[sample(1:.N)]
+  dat <- orderData(dat,4)
+  dat <- as.data.table(dat)
+  setnames(dat,colnames(dat),colnames(dat_orig))
+  all.equal(dat,dat_orig)
+})
+
+table(test)
+
+
+
 
 
 
